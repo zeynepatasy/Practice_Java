b8e412ea7ca556b35c7622d4de5081b5b76b5a6d
zeynepatasy <zeynepatasoy3900@gmail.com>
2023-02-23 19:15:00 +0300

    deneme yazısı

--- IdeaProjects/Practice_Java/src/Concatenation.java
+++ IdeaProjects/Practice_Java/src/Concatenation.java
@@ -1,5 +1,7 @@
 public class Concatenation {
     public static void main(String[] args) {
         //Sadece verilen variable’lari kullanarak istenen String’leri elde edelim
+
+        System.out.println("deneme yapıyorum");
     }
 }
