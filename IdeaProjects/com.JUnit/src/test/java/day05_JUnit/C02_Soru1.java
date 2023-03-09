1360c76c64f42079fa70dd8746d77dec2932e6be
zeynepatasy <zeynepatasoy3900@gmail.com>
2023-03-09 03:22:42 +0300

    JUnit devam

---
+++ IdeaProjects/com.JUnit/src/test/java/day05_JUnit/C02_Soru1.java
@@ -0,0 +1,35 @@
+package day05_JUnit;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C02_Soru1 {
+    public static void main(String[] args) {
+        //signin butonuna tıkla
+        //login alanına "username" yazdır
+        //password alanına "password" yazdır
+        //sign in butonuna tıklayın
+        //Pay Bills sayfasına gidin
+        //amount kısmına yatırmak istedğiniz herhangi bir miktar giriniz
+        //tarih kısmına "2020-09-10" yazdırın
+        //pay butonuna tıklayın
+        //"The  payment was successfully submitted" mesajının çıktıgını kontrol edin
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        //https://zero.webappsecurity.com sayfasına git
+        driver.get("https://zero.webappsecurity.com");
+        // güvenlik var..
+        driver.navigate().back();
+        driver.navigate().refresh();
+        driver.navigate().back();
+        driver.navigate().back();
+
+
+    }
+}
---
+++ IdeaProjects/com.JUnit/src/test/java/day05_JUnit/C04_BeforeAfter.java
@@ -0,0 +1,38 @@
+package day05_JUnit;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C04_BeforeAfter {
+    WebDriver driver;
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+   @After
+   public void tearDown(){
+        driver.close();
+    }
+    @Test
+    //her methodun basında before method call, sonunda after method call varmıs gibidir.Her defasında kontrol eder.
+    public void test1(){
+        driver.get("https://www.amazon.com");
+    }
+    @Test
+    public void test2(){
+        driver.get("https://www.facebook.com");
+    }
+    @Test
+    public void test3(){
+        driver.get("https://www.techproeducation.com");
+    }
+}
